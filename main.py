15b637cce4df52024bb2c4c17cc9a41573fd2496
xuanzhi33 <xuanzhi33@qq.com>
2022-01-08 13:47:30 +0800

    基本框架

--- main.py
+++ main.py
@@ -14,22 +14,21 @@ chrome_options.add_argument('--headless')
 if __name__ == "__main__":
     print("Running In Web Mode")
     env = enviroment.Env()
-    img = Image.new("RGB", (296,128), "white")
+    img = Image.open("resources/about.png")
     env.Screen.display(img)
     browser = webdriver.Chrome(options=chrome_options)
 
     browser.get("https://playground.xuanzhi33.cn/pi-core/main/")
     # 设置页面大小为296x128
     browser.set_window_size(296, 128)
-    print("okk")
-    sleep(0.8)
+    sleep(0.5)
     screenshot = browser.get_screenshot_as_png()
 
     screenshotImg = Image.open(BytesIO(screenshot))
-    env.Screen.display_auto(screenshotImg)
+    env.Screen.display(screenshotImg)
  
     touch_recoder_dev = enviroment.touchscreen.TouchRecoder()
     touch_recoder_old = enviroment.touchscreen.TouchRecoder()
     while 1:
         env.Touch.icnt_scan(touch_recoder_dev, touch_recoder_old)
-        print(touch_recoder_dev.X, touch_recoder_dev.Y)
+        print(touch_recoder_dev.Touch, touch_recoder_dev.X[0], touch_recoder_dev.Y[0])
