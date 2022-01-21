8fc739a4eca39718b16d56a732ac2c1ec39656cf
xuanzhi33 <xuanzhi33@qq.com>
2022-01-21 17:51:50 +0800

    无网络时可扫描wifi

--- main.py
+++ main.py
@@ -32,8 +32,9 @@ def get_host_ip():
         s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         s.connect(('8.8.8.8', 80))
         ip = s.getsockname()[0]
-    finally:
         s.close()
+    except:
+        ip = "无法获取IP"
 
     return ip
 
